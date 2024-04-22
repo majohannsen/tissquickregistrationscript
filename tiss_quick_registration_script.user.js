2181e28baf4370c428eb5417f16655375a59205a
Mathias Johannsen <mathias.j@arkulpa.at>
2024-04-22 18:35:58 +0200

    added version note

--- tiss_quick_registration_script.user.js
+++ tiss_quick_registration_script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name       TISS Quick Registration Script (edited)
+// @name       TISS Quick Registration Script
 // @namespace  http://www.manuelgeier.com/
-// @version    1.6.3
+// @version    1.7.0 (Fork)
 // @description  Script to help you to get into the group you want. Opens automatically the right panel, registers automatically and confirms your registration automatically. If you don't want the script to do everything automatically, the focus is already set on the right button, so you only need to confirm. There is also an option available to auto refresh the page, if the registration button is not available yet, so you can open the site and watch the script doing its work. You can also set a specific time when the script should reload the page and start.
 // @match      https://tiss.tuwien.ac.at/*
 // @copyright  2020 Manuel Geier, MIT License
@@ -35,6 +35,10 @@ SOFTWARE.
 /*
  Changelog:
  
+ v.1.7.0 [22.04.2024] (Fork)
+ + Added: select time slot support for exams
+ + Added: you can choose the reload rate
+
  v.1.6.3 [18.12.2020]
  + Added: date of exam support
 
